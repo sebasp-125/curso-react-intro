3dac5624f2443d8bf4e6265355d742643fa205ed
sebasp-125 <sp6027193@gmail.com>
2024-01-11 18:37:28 -0500

    CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
+import './App.css'; // Importando los estilos CSS
+
 const defaultTodos = [
   {
     text: 'Cortar Cebolla', completed: false
@@ -28,18 +30,25 @@ function App() {
   return (
     <>
       <TodoCounter completed={16} total={25} />
-      <TodoSearch />
-
+     
       <TodoList>
+        <div className='container_container'>
+        <div className='container'>
+          <h2 className='Todo_Title_Container'>Task List</h2>
+          <TodoSearch /> {TodoSearch}
         {defaultTodos.map(todo => (
           <TodoItem key={todo.text}// El Key(Llave) va ser los diferetes TodoItems text: 
            text={todo.text} //Esta enviando los TodoItems a Todoitem.js
            completed = {todo.completed} />  //Ha futuro. Marcar completado o Faltante
         ))} 
+        </div>
+        </div>
+        
       </TodoList>
 
       <CreateTodoButton />
-    </>
+    </> 
+   
   );
 }
 
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,14 +2,13 @@ import './todoCounter.css'; // Importando los estilos CSS
 
 function TodoCounter({ total, completed }) {
   return (
-    <body>
- <div>
-      <h2 className="title">To-Do List</h2>
+ <div className='content_titles'>
+  <h2 className='Build'>Build</h2>
+      <h2 className="title"><span className='To-Do'>To-Do</span> <span className='List'>List</span></h2>
       <h1>
-        Has completado <span>{completed}</span> de <span>{total}</span> To Do List
+        Has completado <span className='completed_total'>{completed}</span> de <span className='completed_total'>{total}</span> To Do List
       </h1>
     </div>
-    </body>
   );
 }
 
